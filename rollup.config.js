96b0a3b644509a35a35b4cd7019be12d543222a7
DavideViolante <fungtq3@gmail.com>
2020-07-22 23:16:41 +0200

    Fix build file name

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/main.js',
   output: [
-    { file: 'build/sheet2api-js.js', format: 'umd', name: 'sheet2api' },
-    { file: 'build/sheet2api-js.min.js', format: 'umd', name: 'sheet2api', plugins: [terser()] },
+    { file: 'build/sheet2api-js.js', format: 'umd', name: 'Sheet2API', plugins: [terser()] },
   ],
 };
